2b103c2bd6d3b0896a2491bf29f14567806cbb15
remove84 <58025631+remove84@users.noreply.github.com>
2023-11-09 17:35:01 +0100

    mit

--- src/main/java/kth/alex/demo/repository/DoctorRepository.java
+++ src/main/java/kth/alex/demo/repository/DoctorRepository.java
@@ -2,7 +2,6 @@ package kth.alex.demo.repository;
 
 import kth.alex.demo.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/kth/alex/demo/repository/OtherPersonalRepository.java
+++ src/main/java/kth/alex/demo/repository/OtherPersonalRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OtherPersonalRepository extends JpaRepository<OtherPersonal, String> {
-    @Query("SELECT o FROM OtherPersonal o WHERE o.socialNr = :socialNr")
+    @Query("SELECT o FROM OtherPersonal o WHERE o.socialNr = ?1")
     OtherPersonal findBySocialNr(String socialNr);
 }
 
